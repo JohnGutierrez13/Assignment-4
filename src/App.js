60b8a94bc0de838541c1d1b3cc696090f52d9126
John Gutierrez <John.Gutierrez28@qmail.cuny.edu>
2022-04-17 12:01:06 -0400

    Configure React app for deployment to GitHub Pages last attenmpt

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {HashRouter as Router, Route} from 'react-router-dom';
 import Home from './components/Home';
 import UserProfile from './components/UserProfile';
 import LogIn from './components/Login';
