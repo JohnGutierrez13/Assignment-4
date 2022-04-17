f07a7c8fa6dc5eff1cf9650587195c18a0e2cb88
John Gutierrez <John.Gutierrez28@qmail.cuny.edu>
2022-04-17 01:26:09 -0400

    Up-Dating UserProfile

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -2,18 +2,24 @@
 // The UserProfile component is used to demonstrate the use of Route and Link.
 
 import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 class UserProfile extends Component {
+  getDate=(localDate)=>{ return localDate.slice(0,9) } 
   render() {
     return (
+      
         <div>
-          <h1>User Profile</h1>
-
-          <div>Username: {this.props.userName}</div>
-          <div>Member Since: {this.props.memberSince}</div>
           
-          <Link to="/">Return to Home</Link>
+          <br></br>
+          <NavLink to="/" activeClassName="is-active" style={{color: "#ffe54c"}}>Return to Home</NavLink>
+          <br></br><br></br>
+          <h1>User Profile</h1>
+          <br></br>
+          <div>Username: {this.props.userName}</div>
+          <div>Member Since: {this.getDate(this.props.memberSince)}</div>
+          <br></br>
+          
         </div>
     );
   }
